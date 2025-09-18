0aed0bb3c9a9e145b8f714dd9ece99bb2a45840b
jaideep1836 <jaideepwarring12@gmail.com>
2025-09-18 18:17:12 +0530

    Update DecentraDocs.sol

--- contracts/DecentraDocs.sol
+++ contracts/DecentraDocs.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
